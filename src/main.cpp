5a24121f7d63ca1b8f94b073fc8a73f31ec167d8
Shinagi47 <sophiehgain@gmail.com>
2026-03-23 01:14:04 -0400

    completed genre filtering

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,73 @@ int main() {
     animes = parseDataset2("../database/rating-1.csv", animes); //parce anime.csv. will parce rating.csv too
     animes = parseDataset2("../database/rating-2.csv", animes); //parce anime.csv. will parce rating.csv too
 
-    //TESTING
-    for (int i = 0; i < animes.size(); i++) {
-        std::cout << animes[i].title << ", " << animes[i].genre << ", " << fixed<<setprecision(2)<<animes[i].score << ", " << animes[i].number_of_ratings << std::endl; //testing
-    }
-
     //if action genre selected... if romance genre selected... etc
     //new array of only that genre ?
-    //vector<anime> filteredAnimes = byGenre(animes, "Action");
+
+    while (true) {
+        cout << "Enter a genre:";
+        string genre;
+        cin >> genre;
+        for (int i = 0; i < genre.size(); i++) {
+            if (i == 0) {
+                genre[0] = toupper(genre[0]);
+            }
+            else {
+                genre[i] = tolower(genre[i]);
+            }
+        }
+
+        std::vector<anime> filtered = animes;
+
+        filtered = filterByGenre(animes, genre);
+        //TESTING
+        for (int i = 0; i < filtered.size(); i++) {
+            std::cout << filtered[i].title << ": " << filtered[i].genre << ", " << fixed<<setprecision(2)<<filtered[i].score << ", " << filtered[i].number_of_ratings << std::endl; //testing
+        }
+        filtered = animes;
+    }
+
+    /*
+    if (genre == "action") {
+
+    }
+    else if (genre == "adventure") {
+
+    }
+    else if (genre == "comedy") {
+
+    }
+    else if (genre == "drama") {
+
+    }
+    else if (genre == "fantasy") {
+
+    }
+    else if (genre == "historical") {
+
+    }
+    else if (genre == "horror") {
+
+    }
+    else if (genre == "mystery") {
+
+    }
+    else if (genre == "romance") {
+
+    }
+    else if (genre == "sci-fi") {
+
+    }
+    else if (genre == "slice of life") {
+
+    }
+    else if (genre == "supernatural") {
+
+    }
+    else if (genre == "thriller") {
+
+    }
+    */
 
     //if heapsort selected... take in struct vector
     //call heapsort based on niche score values
